f24f71471175f663e8b6b902d8e70f87b6356fc3
OskinDI <denis_oskin@rambler.ru>
2021-02-28 12:15:48 +0300

    доработка

--- src/main/java/com/oskin_denis/java/filemanager/Controller.java
+++ src/main/java/com/oskin_denis/java/filemanager/Controller.java
@@ -2,26 +2,61 @@ package com.oskin_denis.java.filemanager;
 
 
 import javafx.application.Platform;
-import javafx.beans.property.SimpleObjectProperty;
-import javafx.beans.property.SimpleStringProperty;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.fxml.Initializable;
-import javafx.scene.control.*;
-import javafx.util.Callback;
+import javafx.scene.control.Alert;
+import javafx.scene.control.ButtonType;
+import javafx.scene.layout.VBox;
 
 import java.io.IOException;
-import java.net.URL;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
-import java.time.format.DateTimeFormatter;
-import java.util.ResourceBundle;
-import java.util.stream.Collectors;
+
 
 public class Controller {
+
+    @FXML
+    VBox leftPanel, rightPanel;
+
+
     public void btnExitAction(ActionEvent actionEvent) {
         Platform.exit();
     }
 
+
+    public void copyBtnAction(ActionEvent actionEvent) {
+        PanelController leftPC = (PanelController) leftPanel.getProperties().get("ctrl");
+        PanelController rightPC = (PanelController) rightPanel.getProperties().get("ctrl");
+
+        if (leftPC.getSelectedFilename() == null && rightPC.getSelectedFilename() == null) {
+            Alert alert = new Alert(Alert.AlertType.ERROR, "Ни один файл не выбран", ButtonType.OK);
+            alert.showAndWait();
+            return;
+        }
+
+        PanelController srcPC = null, dstPC = null;
+        if (leftPC.getSelectedFilename() != null) {
+            srcPC = leftPC;
+            dstPC = rightPC;
+        }
+
+        if (rightPC.getSelectedFilename() != null) {
+            srcPC = rightPC;
+            dstPC = leftPC;
+        }
+
+        Path srcPath = Paths.get(srcPC.getCurrentPath(),srcPC.getSelectedFilename());
+        Path dstPath = Paths.get(dstPC.getCurrentPath()).resolve(srcPath.getFileName().toString());
+
+        try {
+            Files.copy(srcPath, dstPath);
+            dstPC.updateList(Paths.get(dstPC.getCurrentPath()));
+        } catch (IOException e) {
+            Alert alert = new Alert(Alert.AlertType.ERROR, "Ни удалось скопировать указанный файл",ButtonType.OK);
+            alert.showAndWait();
+        }
+
+
+    }
 }
