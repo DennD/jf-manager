a85381924c9c03ee71c005d9c27f2f7ecca98b64
Denis <darkuz.rush@gmail.com>
2021-03-01 11:46:51 +0300

    OK!

--- src/main/java/com/oskin_denis/java/filemanager/Controller.java
+++ src/main/java/com/oskin_denis/java/filemanager/Controller.java
@@ -8,6 +8,7 @@ import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.*;
+import javafx.scene.layout.VBox;
 import javafx.util.Callback;
 
 import java.io.IOException;
@@ -20,8 +21,134 @@ import java.util.ResourceBundle;
 import java.util.stream.Collectors;
 
 public class Controller {
+
+    @FXML
+    VBox rightPanel, leftPanel;
+
     public void btnExitAction(ActionEvent actionEvent) {
         Platform.exit();
     }
 
+    public void copyBtnAction(ActionEvent actionEvent){
+
+        PanelController leftPC = (PanelController) leftPanel.getProperties().get("ctrl");
+        PanelController rightPC = (PanelController) rightPanel.getProperties().get("ctrl");
+
+        if(leftPC.getSelectedFilename() == null && rightPC.getSelectedFilename() == null) {
+            Alert alert = new Alert(Alert.AlertType.ERROR, "Ни один файл не выбран", ButtonType.OK);
+            alert.showAndWait();
+            return;
+        }
+
+        Path srcPath = null;
+        Path dstPath = null;
+
+        PanelController srcPC = null, dstPC = null;
+        if (leftPC.getSelectedFilename() != null){
+            srcPC = leftPC;
+            dstPC = rightPC;
+        }
+
+        if(rightPC.getSelectedFilename() != null){
+            srcPC = rightPC;
+            dstPC = leftPC;
+        }
+
+            srcPath = Paths.get(srcPC.getCurrentPath(),srcPC.getSelectedFilename());
+            dstPath = Paths.get(dstPC.getCurrentPath()).resolve(srcPath.getFileName().toString());
+
+
+        try {
+            Files.copy(srcPath,dstPath);
+            dstPC.updateList(Paths.get(dstPC.getCurrentPath()));
+        } catch (IOException e) {
+            Alert alert = new Alert(Alert.AlertType.ERROR, "Не удалось скопировать указанный файл", ButtonType.OK);
+            alert.showAndWait();
+        }
+
+
+    }
+
+    public void deleteBtnAction(ActionEvent actionEvent) {
+
+        PanelController leftPC = (PanelController) leftPanel.getProperties().get("ctrl");
+        PanelController rightPC = (PanelController) rightPanel.getProperties().get("ctrl");
+
+        if(leftPC.getSelectedFilename() == null && rightPC.getSelectedFilename() == null) {
+            Alert alert = new Alert(Alert.AlertType.ERROR, "Ни один файл не выбран", ButtonType.OK);
+            alert.showAndWait();
+            return;
+        }
+
+        Path srcPath = null;
+
+        PanelController srcPC = null;
+        PanelController dstPC = null;
+
+        if (leftPC.getSelectedFilename() != null){
+            srcPC = leftPC;
+            dstPC = rightPC;
+        }
+
+        if(rightPC.getSelectedFilename() != null){
+            srcPC = rightPC;
+            dstPC = leftPC;
+        }
+
+        srcPath = Paths.get(srcPC.getCurrentPath(),srcPC.getSelectedFilename());
+
+
+
+        try {
+            Files.delete(srcPath);
+            srcPC.updateList(Paths.get(srcPC.getCurrentPath()));
+            dstPC.updateList(Paths.get(dstPC.getCurrentPath()));
+
+        } catch (IOException e) {
+            Alert alert = new Alert(Alert.AlertType.ERROR, "Не удалось удалить указанный файл", ButtonType.OK);
+            alert.showAndWait();
+        }
+
+
+    }
+
+    public void moveBtnAction(ActionEvent actionEvent) {
+
+        PanelController leftPC = (PanelController) leftPanel.getProperties().get("ctrl");
+        PanelController rightPC = (PanelController) rightPanel.getProperties().get("ctrl");
+
+        if(leftPC.getSelectedFilename() == null && rightPC.getSelectedFilename() == null) {
+            Alert alert = new Alert(Alert.AlertType.ERROR, "Ни один файл не выбран", ButtonType.OK);
+            alert.showAndWait();
+            return;
+        }
+
+        Path srcPath = null;
+        Path dstPath = null;
+
+        PanelController srcPC = null, dstPC = null;
+        if (leftPC.getSelectedFilename() != null){
+            srcPC = leftPC;
+            dstPC = rightPC;
+        }
+
+        if(rightPC.getSelectedFilename() != null){
+            srcPC = rightPC;
+            dstPC = leftPC;
+        }
+
+        srcPath = Paths.get(srcPC.getCurrentPath(),srcPC.getSelectedFilename());
+        dstPath = Paths.get(dstPC.getCurrentPath()).resolve(srcPath.getFileName().toString());
+
+
+        try {
+            Files.move(srcPath,dstPath);
+            srcPC.updateList(Paths.get(srcPC.getCurrentPath()));
+            dstPC.updateList(Paths.get(dstPC.getCurrentPath()));
+        } catch (IOException e) {
+            Alert alert = new Alert(Alert.AlertType.ERROR, "Не удалось переместить указанный файл", ButtonType.OK);
+            alert.showAndWait();
+        }
+
+    }
 }
--- src/main/java/com/oskin_denis/java/filemanager/Main.java
+++ src/main/java/com/oskin_denis/java/filemanager/Main.java
@@ -4,6 +4,7 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
 
 public class Main extends Application {
@@ -11,7 +12,8 @@ public class Main extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("/main.fxml"));
-        primaryStage.setTitle("Java File Manager [GeekBrains]");
+        primaryStage.getIcons().add(new Image("/icon.png"));
+        primaryStage.setTitle("Portfile");
         primaryStage.setScene(new Scene(root, 1012, 600));
         primaryStage.show();
     }
